009eebfe49586e02a6e0c8ff48201ae27edf3c2a
John Cai <CaiJianbin93@gmail.com>
2023-01-30 17:02:33 +0800

    update

--- csv2xml.py
+++ csv2xml.py
@@ -14,41 +14,46 @@ def csv2xml(pdObj, sequenceId):
     for index, row in pdObj.iterrows():
         if index == 0:
             continue
-        party = ET.SubElement(parties, "DPPartyToScreen")
-        ET.SubElement(party, "LinkageID").text = "0"
-        ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
-        ET.SubElement(party, "SourceSystemSubKey").text = "VES"
-        ET.SubElement(party, "PartyName").text = row[1].strip() # VesselName
-        ET.SubElement(party, "CountryOfExport").text = row[2].strip() # POL
-        ET.SubElement(party, "PartyCountryCode")
+        if len(row[1].strip()) > 0:
+            party = ET.SubElement(parties, "DPPartyToScreen")
+            ET.SubElement(party, "LinkageID").text = "0"
+            ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
+            ET.SubElement(party, "SourceSystemSubKey").text = "VES"
+            ET.SubElement(party, "PartyName").text = row[1].strip() # VesselName
+            ET.SubElement(party, "CountryOfExport").text = row[2].strip() # POL
+            ET.SubElement(party, "PartyCountryCode").text = ' '
 
-        party = ET.SubElement(parties, "DPPartyToScreen")
-        ET.SubElement(party, "LinkageID").text = "0"
-        ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
-        ET.SubElement(party, "SourceSystemSubKey").text = "S"
-        ET.SubElement(party, "PartyName").text = row[4].strip() # Shipper
-        ET.SubElement(party, "PartyCountryCode")
+        if len(row[4].strip()) > 0:
+            party = ET.SubElement(parties, "DPPartyToScreen")
+            ET.SubElement(party, "LinkageID").text = "0"
+            ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
+            ET.SubElement(party, "SourceSystemSubKey").text = "S"
+            ET.SubElement(party, "PartyName").text = row[4].strip() # Shipper
+            ET.SubElement(party, "PartyCountryCode").text = ' '
 
-        party = ET.SubElement(parties, "DPPartyToScreen")
-        ET.SubElement(party, "LinkageID").text = "0"
-        ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
-        ET.SubElement(party, "SourceSystemSubKey").text = "S"
-        ET.SubElement(party, "PartyName").text = row[7].strip() # Customer
-        ET.SubElement(party, "PartyCountryCode")
+        if len(row[7].strip()) > 0:
+            party = ET.SubElement(parties, "DPPartyToScreen")
+            ET.SubElement(party, "LinkageID").text = "0"
+            ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
+            ET.SubElement(party, "SourceSystemSubKey").text = "S"
+            ET.SubElement(party, "PartyName").text = row[7].strip() # Customer
+            ET.SubElement(party, "PartyCountryCode").text = ' '
 
-        party = ET.SubElement(parties, "DPPartyToScreen")
-        ET.SubElement(party, "LinkageID").text = "0"
-        ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
-        ET.SubElement(party, "SourceSystemSubKey").text = "C"
-        ET.SubElement(party, "PartyName").text = row[5].strip() # Consignee
-        ET.SubElement(party, "PartyCountryCode")
+        if len(row[5].strip()) > 0:
+            party = ET.SubElement(parties, "DPPartyToScreen")
+            ET.SubElement(party, "LinkageID").text = "0"
+            ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
+            ET.SubElement(party, "SourceSystemSubKey").text = "C"
+            ET.SubElement(party, "PartyName").text = row[5].strip() # Consignee
+            ET.SubElement(party, "PartyCountryCode").text = ' '
 
-        party = ET.SubElement(parties, "DPPartyToScreen")
-        ET.SubElement(party, "LinkageID").text = "0"
-        ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
-        ET.SubElement(party, "SourceSystemSubKey").text = "N"
-        ET.SubElement(party, "PartyName").text = row[6].strip() # NotifyParty
-        ET.SubElement(party, "PartyCountryCode")
+        if len(row[6].strip()) > 0:
+            party = ET.SubElement(parties, "DPPartyToScreen")
+            ET.SubElement(party, "LinkageID").text = "0"
+            ET.SubElement(party, "SourceSystemUniqueKey").text = row[3].strip() # HBL
+            ET.SubElement(party, "SourceSystemSubKey").text = "N"
+            ET.SubElement(party, "PartyName").text = row[6].strip() # NotifyParty
+            ET.SubElement(party, "PartyCountryCode").text = ' '
 
 
     tree = ET.ElementTree(root)
